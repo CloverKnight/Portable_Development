38350e06513e1797df0acbdeb8d10d74974e8f0b
Alex Ireland <ireland.alex@yahoo.ca>
2017-02-15 08:03:00 -0700

    Added another path prompt option. Layed out basic script structure.

--- Portable_Development_Setup.ps1
+++ Portable_Development_Setup.ps1
@@ -4,7 +4,7 @@
 
 
 #Prompt for a path
-function Prompt-Path ([String] $Description)
+function Prompt-Path-Dialog ([String] $Description)
 {
 	$FolderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
 	
@@ -16,3 +16,31 @@ function Prompt-Path ([String] $Description)
 	$FolderBrowser.ShowDialog()
 	return $FolderBrowser.SelectedPath
 }
+
+function Prompt-Path
+{
+	$Path = Read-Host -Prompt 'Enter an installation path'
+	$ValidPath = Test-Path $Path
+	
+	if (!$ValidPath)
+	{
+		Write-Error "Invalid path entered"		
+	}
+	
+	return $Path
+}
+
+#Prompt the user for an installation path
+$Folder = Prompt-Path "Select an installation folder"
+Write-Host "Installing path" $Folder
+
+
+
+#Create file structure
+
+#Get default package installation path for Chocolatey and save it
+#Set the default package installation path to the user selected path
+#Install packages
+#Reset the default package installation path
+
+#Set up personalizations
