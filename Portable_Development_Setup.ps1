c68735b865a7b5b1984a6e564de82a16bba43f4c
Alex Ireland <ireland.alex@yahoo.ca>
2017-02-16 08:51:29 -0700

    Added a confirmation prompt function. Started work on the file structure.

--- Portable_Development_Setup.ps1
+++ Portable_Development_Setup.ps1
@@ -1,5 +1,6 @@
 #Name: Portable Development Setup
-#Last updated: 2/13/2017
+#Created: 2/13/2017
+#Last updated: 2/16/2017
 #Author: Alex Ireland
 
 
@@ -27,16 +28,44 @@ function Prompt-Path
 		Write-Error "Invalid path entered"		
 	}
 	
-	return $Path
+	return $Path.TrimEnd('\');
+}
+
+function Prompt-Confirmation([String] $Message) 
+{
+	$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes"
+	$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No"
+	$options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
+	$result = $host.ui.PromptForChoice($title, $message, $options, 0)
+	
+	switch ($result)
+	{
+		0 {return $true} 
+		1 {return $false}
+	}
+	
+	return $result
 }
 
 #Prompt the user for an installation path
 $Folder = Prompt-Path "Select an installation folder"
 Write-Host "Installing path" $Folder
 
-
-
 #Create file structure
+#
+#Installation Directory
+#|_Tools
+#  |_Cmder
+#  |_VSCode
+#|_Projects
+#|_Sandbox
+
+$confirmInstallation = Prompt-Confirmation("Installation will delete all contents of the installation path. Are you sure you want to install?")
+
+if ($confirmInstallation)
+{
+	New-Item $Folder\Tools -type Directory
+}
 
 #Get default package installation path for Chocolatey and save it
 #Set the default package installation path to the user selected path
